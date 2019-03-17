5a9c91b31e5f98e167797e2fecab63cbba43de13
Dalton Mooring <daltonmooring@gmail.com>
2019-03-17 03:15:46 -0400

    Putting code where it belongs
    also spelling fix ;) precision

---
+++ PracticeKeeper/js/graph-options.js
@@ -0,0 +1,53 @@
+function generateLink(){
+  var final="?d=";
+  for(var i=1;i!=8;i++){
+    var current= document.getElementById("checkbox"+i).checked;
+    if(current===true) final+="1";
+    if (current===false) final+="0";
+  }
+  final+="&c1="+document.getElementById("color1").value;
+  final+="&c2="+document.getElementById("color2").value;
+  final+="&p="+ $('#slider').slider("option", "value");
+  return final;
+}
+
+var $slider = $("#slider");
+  if ($slider.length > 0) {
+    $slider.slider({
+      min: 0,
+      max: 5,
+      value: 3,
+      orientation: "horizontal",
+      range: "min"
+    });
+  }
+  var num_circles = 20;
+  function default_gradient(){ //creating defeault gradent
+    var color1 = $("#color1").val();
+    var color2 = $("#color2").val();
+    var color_array=generateColor(color2,color1,num_circles+1);
+    for (var i = 0; i < num_circles; i++) {
+      $('.gradient').append('<div id="circle-' + i + '"class="circle"</div>');
+      document.getElementById("circle-"+i).style.backgroundColor = color_array[i];
+    }
+    $('.gradient').append('<br>');
+  }
+  function display_grad(){
+    var color1 = $("#color1").val();
+    var color2 = $("#color2").val();
+    var color_array=generateColor(color2,color1,num_circles+1);
+    for (var i = 0; i < num_circles; i++) {
+      document.getElementById("circle-"+i).style.backgroundColor = color_array[i];
+    }
+  }
+  document.getElementById("nextButton").onclick = function () {
+    var meta_data=generateLink();
+    location.href = "./chart.html"+meta_data;
+  };
+  $("#select_all").click(function () {
+      $('input:checkbox').not(this).prop('checked', this.checked);
+  });
+
+  $(document).ready(function () {
+      $(':checkbox').radiocheck();
+  });
